b9f6b9637e374720929b03113a19923d43884e78
Paulo <paulokowalski@gmail.com>
2025-05-29 08:00:39 -0300

    refatoração

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://192.168.68.116:8080/casa-api/'
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/casa-api/'
 });
 
 // Interceptor para adicionar token de autenticação
